f7429de86fab03e22d67eda3cb0b84e8246a9490
Morten Fæster <morten.faester@jppol.dk>
2022-03-11 12:59:51 +0100

    Stubbing rabbti

---
+++ RabbitAndSqs/RabbitAndSqs.Tests/Connections/sqs/SqsOutgoingTransportTest.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Amazon.S3;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using FluentAssertions;
+using RabbitAndSqs.Connections;
+using RabbitAndSqs.Connections.Messages;
+using RabbitAndSqs.Connections.Sqs;
+using Xunit;
+
+namespace RabbitAndSqs.Tests.Connections.sqs
+{
+    public abstract class SqsOutgoingTransportTest<TModel>
+    {
+        private readonly SqsOutgoingTransport<TModel> _subject;
+        private readonly SqsIncomingTransport<TModel> _receive;
+        private readonly IMessageFactory<TModel> _messageFactory;
+        private readonly AmazonSQSClient _client;
+        private readonly AmazonS3Client _s3;
+        private readonly string _queue;
+        private readonly string _bucket;
+
+        protected SqsOutgoingTransportTest()
+        {
+            _messageFactory = new XmlMessageFactory<TModel>(GetHeaderFunctions().ToArray());
+
+            _client = new AmazonSQSClient(TestConfiguration.GetAwsCredentials(), TestConfiguration.GetAWSRegionEndpoint());
+            _s3 = new AmazonS3Client(TestConfiguration.GetAwsCredentials(), TestConfiguration.GetAWSRegionEndpoint());
+            _queue = TestConfiguration.GetValue("sqs_queue");
+            _bucket = TestConfiguration.GetValue("s3_bucket");
+            _subject = new SqsOutgoingTransport<TModel>(_client, _queue, _s3, _bucket);
+            _receive = new SqsIncomingTransport<TModel>(_client,_queue, _messageFactory, _s3, _bucket);
+
+            ClearQueue();
+        }
+
+        private void ClearQueue()
+        {
+            Task<ReceiveMessageResponse> messageResponse;
+            do
+            {
+                messageResponse = _client.ReceiveMessageAsync(_queue);
+                messageResponse.Wait();
+                _client.DeleteMessageBatchAsync(_queue, messageResponse.Result.Messages.Select(x => new DeleteMessageBatchRequestEntry(x.MessageId, x.ReceiptHandle)).ToList());
+
+            } while (messageResponse.Result.Messages.Any());
+        }
+
+        protected abstract IEnumerable<KeyValuePair<string, Func<TModel, string>>> GetHeaderFunctions();
+        protected abstract TModel CreateModelInstance();
+
+
+        [Fact]
+        public async Task Send_ThenNoApparentException()
+        {
+            ISerializedMessage<TModel> messages = _messageFactory.CreateFrom(CreateModelInstance());
+            await _subject.Send(messages);
+        }
+
+        [Fact]
+        public async Task Send_ThenReceive()
+        {
+            ISerializedMessage<TModel> messages = _messageFactory.CreateFrom(CreateModelInstance());
+            await _subject.Send(messages);
+
+            var items = await _receive.ReceiveBatch(CancellationToken.None);
+            items.Should().NotBeEmpty();
+        }
+
+        [Fact]
+        public async Task Send_ThenCanBeDeserialized()
+        {
+            var originalMessage = CreateModelInstance();
+            ISerializedMessage<TModel> message = _messageFactory.CreateFrom(originalMessage);
+
+            await _subject.Send(message);
+
+            var response = await _receive.ReceiveBatch(CancellationToken.None);
+            var first = response.Select(x => x.Deserialize()).Single();
+            first.Should().BeEquivalentTo(originalMessage, options => options.AllowingInfiniteRecursion());
+        }
+
+        [Fact]
+        public async Task Send_ThenReceiveSendItem()
+        {
+            var originalMessage = CreateModelInstance();
+            ISerializedMessage<TModel> message = _messageFactory.CreateFrom(originalMessage);
+
+            await _subject.Send(message);
+
+            var items = await _receive.ReceiveBatch(CancellationToken.None);
+            var first = items.Single().Deserialize();
+            first.Should().BeEquivalentTo(originalMessage, options => options.AllowingInfiniteRecursion());
+        }
+    }
+}
---
+++ RabbitAndSqs/RabbitAndSqs/Connections/RabbitMqTransport.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace RabbitAndSqs.Connections
+{
+    public class RabbitMqTransport<TModel> : IOutgoingTransport<TModel>, IIncomingTransport<TModel>
+    {
+        private readonly IMessageFactory<TModel> _messageFactory;
+
+        private string _queue;
+        private string _username;
+        private string _password;
+        private string _hostname;
+
+        public RabbitMqTransport(string queue, string username, string password, string hostname, IMessageFactory<TModel> messageFactory)
+        {
+            _queue = queue;
+            _username = username;
+            _password = password;
+            _hostname = hostname;
+            _messageFactory = messageFactory;
+        }
+
+        public Task Send(ISerializedMessage<TModel> message)
+        {
+            using var connection = CreateConnection();
+
+            using var channel = connection.CreateModel();
+            var properties = channel.CreateBasicProperties();
+            foreach (var kvp in message.Headers)
+            {
+                properties.Headers[kvp.Key] = kvp.Value;
+            }
+
+
+            var body = Encoding.UTF8.GetBytes(message.Content);
+
+            channel.QueueDeclare(queue: _queue, durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+            channel.BasicPublish(exchange: "", routingKey: "ignore", basicProperties: properties, body: body);
+
+            return Task.CompletedTask;
+        }
+        // lb.mq-dev.jppol.net:15672/#/
+        // sb
+        // sb
+        private IConnection CreateConnection()
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = _hostname,
+                UserName = _username,
+                Password = _password
+            };
+            return factory.CreateConnection();
+        }
+
+        public Task<IEnumerable<ISerializedMessage<TModel>>> ReceiveBatch(CancellationToken cancellationToken)
+        {
+            using var connection = CreateConnection();
+            using var channel = connection.CreateModel();
+            var result = new List<ISerializedMessage<TModel>>();
+            BasicGetResult lastReceived;
+            int sizeLeft = 10;
+            do
+            {
+                lastReceived = channel.BasicGet(_queue, true);
+                if (lastReceived == null) { continue; }
+
+                var message = _messageFactory.CreateFrom(Encoding.UTF8.GetString(lastReceived.Body.ToArray()), lastReceived.BasicProperties.Headers.ToDictionary(x => x.Key, x=> x.Value?.ToString()));
+                result.Add(message);
+
+            } while (lastReceived != null && sizeLeft-- > 0);
+
+            return Task.FromResult((IEnumerable<ISerializedMessage<TModel>>)result);
+        }
+    }
+}
